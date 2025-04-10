4b8e8be77df1adf735922255c61d52b5a89aa39c
Kjasn <iz.kjasn@icloud.com>
2025-04-10 13:05:47 +0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by YuRuosheng on 2025/4/10.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScrumdingerTests/ScrumdingerTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScrumdingerTests.swift
+//  ScrumdingerTests
+//
+//  Created by YuRuosheng on 2025/4/10.
+//
+
+import Testing
+
+struct ScrumdingerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
